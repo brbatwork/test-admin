adfe15e6d02529e3724d2b5feda96ae866011825
Bill Barbour <bill.barbour@oracle.com>
2018-06-27 14:07:04 -0400

    Used responsive tag

--- src/posts.js
+++ src/posts.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { List, Edit, Create, EditButton, Filter,
 	DisabledInput, LongTextInput, ReferenceInput, SelectInput, TextInput,
-	SimpleForm, Datagrid, TextField,
+	SimpleForm, Datagrid, TextField, Responsive, SimpleList,
 	ReferenceField } from 'react-admin';
 
 
 export const PostList = (props) => (
 	<List {...props} filters={<PostFilter />}>
-		<Datagrid>
-			<TextField source="id" />
-			<ReferenceField label="User" source="userId" reference="users">
-				<TextField source="name" />
-			</ReferenceField>
-			<TextField source="title" />
-			<TextField source="body" />
-			<EditButton />
-		</Datagrid>
+		<Responsive
+			small={
+				<SimpleList primaryText={record => record.title}
+					secondaryText={record => `${record.views} views`}
+					tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+				/>
+			}
+			medium = {
+				<Datagrid>
+					<TextField source="id" />
+					<ReferenceField label="User" source="userId" reference="users">
+						<TextField source="name" />
+					</ReferenceField>
+					<TextField source="title" />
+					<TextField source="body" />
+					<EditButton />
+				</Datagrid>
+			}
+		/>
 	</List>
 );
 
